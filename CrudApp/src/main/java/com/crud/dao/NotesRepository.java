e3d9d0e2ad3adfb9c924bf234112b7efffcbc8a0
Diego Maŕquez <desarrollo.marquez@gmail.com>
2019-01-10 23:05:15 -0500

    Adicion nuevo proyecto

---
+++ CrudApp/src/main/java/com/crud/dao/NotesRepository.java
@@ -0,0 +1,13 @@
+package com.crud.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.crud.domain.Notes;
+
+/**
+ * Created by abraun on 23/11/2017.
+ */
+@Repository
+public interface NotesRepository extends JpaRepository<Notes, Long> {
+}
---
+++ CrudApp/src/main/java/com/crud/service/NotesServiceImpl.java
@@ -0,0 +1,39 @@
+package com.crud.service;
+
+import com.crud.dao.NotesRepository;
+import com.crud.domain.Notes;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * Created by abraun on 23/11/2017.
+ */
+@Service
+public class NotesServiceImpl implements NotesService {
+
+    @Autowired
+    private NotesRepository notesRepository;
+
+    @Override
+    public List<Notes> findAll() {
+        return notesRepository.findAll();
+    }
+
+    @Override
+    public Notes findOne(Long id) {
+        return notesRepository.findOne(id);
+    }
+
+    @Override
+    public Notes saveNotes(Notes notes) {
+        return notesRepository.save(notes);
+    }
+
+    @Override
+    public void deleteNotes(Long id) {
+        notesRepository.delete(id);
+    }
+}
